a790ee38231eacc6d8538466054700e3d541218b
JiyeonP <q0o0p@snu.ac.kr>
2022-11-03 01:24:52 +0900

    add react-no-ssr

--- udong-frontend/pages/_app.tsx
+++ udong-frontend/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import NoSSR from 'react-no-ssr'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+    return <NoSSR>
+        <Component {...pageProps} />
+    </NoSSR>
 }
