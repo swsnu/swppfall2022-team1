044f3d0807fe4ccd81371c635390ecff40fe2376
JiyeonP <q0o0p@snu.ac.kr>
2022-11-03 05:17:10 +0900

    put unsettled event list

--- udong-frontend/app/ui/feature/club/event/calendar/EventCalendarView.tsx
+++ udong-frontend/app/ui/feature/club/event/calendar/EventCalendarView.tsx
@@ -1,15 +1,103 @@
+import { useRouter } from 'next/router'
+
 import { Spacer } from '../../../../components/Spacer'
-import { VStack } from '../../../../components/Stack'
+import { HStack, VStack } from '../../../../components/Stack'
 import { UdongColors } from '../../../../theme/ColorPalette'
 import { EventCalendar } from './EventCalendar'
+import UnsettledEventList from './UnsettledEventList'
+
+export interface EventType {
+    id:string
+    title:string
+    times: {
+        start: string
+        end: string
+    }[]
+}
+
+const dummyEvents: EventType[] = [
+    {
+        id: '1',
+        title: 'event1',
+        times: [{
+            start: '2022-11-01T10:00:00',
+            end: '2022-11-03T11:00:00',
+        }],
+    },
+    {
+        id: '2',
+        title: 'event2',
+        times: [{
+            start: '2022-11-01T10:00:00',
+            end: '2022-11-03T11:00:00',
+        }],
+    },
+    {
+        id: '3',
+        title: 'event3',
+        times: [],
+    },
+    {
+        id: '4',
+        title: 'event4',
+        times: [],
+    },
+    {
+        id: '5',
+        title: 'event5',
+        times: [{
+            start: '2022-11-01T10:00:00',
+            end: '2022-11-03T11:00:00',
+        }, {
+            start: '2022-11-12T10:00:00',
+            end: '2022-11-20T23:00:00',
+        }],
+    },
+    {
+        id: '6',
+        title: 'event6',
+        times: [],
+    },
+    {
+        id: '7',
+        title: 'event7',
+        times: [{
+            start: '2022-11-01T10:00:00',
+            end: '2022-11-03T11:00:00',
+        }],
+    },
+]
 
 export const EventCalendarView = () => {
+    const router = useRouter()
+    const { clubId } = router.query
+
+    const onClickEvent = (eventId: string) => {
+        router.push(`/club/${clubId}/event/${eventId}`)
+    }
+
     return <VStack>
         <Spacer
             height={1}
             backgroundColor={UdongColors.GrayDark}
         />
-        <p>여기는 캘린더 + 날짜 없는 행사 목록 컴포넌트 둘다 보여주는 view, 일단 무시해도 됨</p>
-        <EventCalendar/>
+        <HStack
+            width={'100%'}
+            style={{
+                maxHeight: 'calc(100vh - 130px)',
+                minWidth: '810px',
+            }}
+            justifyContent={'space-between'}
+            gap={50}
+        >
+            <EventCalendar
+                events={dummyEvents.filter((event) => event.times.length > 0)}
+                onClickEvent={onClickEvent}
+            />
+            <UnsettledEventList
+                events={dummyEvents.filter((event) => event.times.length == 0)}
+                onClickEvent={onClickEvent}
+            />
+        </HStack>
     </VStack>
 }
