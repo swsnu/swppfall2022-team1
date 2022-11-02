8fb6339a1b839a77995c3fee149d5796dca97b63
JiyeonP <q0o0p@snu.ac.kr>
2022-11-03 01:20:51 +0900

    making calendar buttons rebase

--- udong-frontend/app/ui/feature/club/event/calendar/TUICalendar.tsx
+++ udong-frontend/app/ui/feature/club/event/calendar/TUICalendar.tsx
@@ -2,7 +2,7 @@
 import '@toast-ui/calendar/dist/toastui-calendar.css'
 import { EventObject } from '@toast-ui/calendar/types/types/events'
 import ToastUIReactCalendar from '@toast-ui/react-calendar'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { UdongColors } from '../../../../theme/ColorPalette'
 
@@ -15,30 +15,97 @@ const calendars = [
     }]
 
 const template = {
+    monthGridHeaderExceed(){
+        return ''
+    },
+    monthGridFooterExceed(count: number) {
+        return `<span>${count} more</span>`
+    },
 }
 
-type CalenderProps = {
+const theme = {
+    common: {
+        today: {
+            backgroundColor: 'white',
+        },
+    },
+    month: {
+        moreView: {
+            border: 'none',
+            boxShadow: '0 2px 3px 0 rgba(0,0,0,0.25)',
+            height: 'auto',
+            paddingBottom: '25px',
+        },
+        gridCell: {
+            headerHeight: 35,
+            footerHeight: 35,
+        },
+    },
+}
+
+const randomPrimaryColor = (seed: number) => {
+    if (seed % 2){
+        return `hsl(234,50%,${45 + (Math.random() * 20)}%)`
+    } else {
+        return `hsl(234,50%,${75 + (Math.random() * 20)}%)`
+    }
+}
+
+interface CalenderProps {
     events: EventObject[]
 }
 
 export const Calender = ( { events } : CalenderProps ) => {
-    const calenderRef = useRef<ToastUIReactCalendar>(null)
+    const calendarRef = useRef<ToastUIReactCalendar>(null)
+    const [date, setDate] = useState<{ year: number, month: number }>()
+    const coloredEvents: EventObject[] = events.map((event)=>({
+        id: `${event.id}`, calendarId: '0', title: event.title,
+        backgroundColor: randomPrimaryColor(event.id), borderColor: 'rgba(0,0,0,0)', color: event.id % 2 ? 'white' : 'black',
+        start: event.start, end: event.end }),
+    )
+
+    const syncDate = () => {
+        if (calendarRef.current && calendarRef.current.getInstance() !== null) {
+            setDate({ year: calendarRef.current.getInstance().getDate().getFullYear(),
+                month: calendarRef.current.getInstance().getDate().getMonth() })}
+    }
+
+    useEffect(() => {
+        syncDate()
+    }, [calendarRef.current])
 
     return (
         <div>
-            <button onClick={()=> {calenderRef.current?.getInstance()?.prev()}}>prev</button>
-            <button onClick={()=> {calenderRef.current?.getInstance()?.today()}}>today</button>
-            <button onClick={()=> {calenderRef.current?.getInstance()?.next()}}>next</button>
+            <button
+                onClick={()=> {
+                    calendarRef.current?.getInstance()?.prev()
+                    syncDate()
+                }}
+            >prev</button>
+            <button
+                onClick={()=> {
+                    calendarRef.current?.getInstance()?.today()
+                    syncDate()
+                }}
+            >today</button>
+            <span>{date.year}.{date.month ? date.month + 1 : undefined}</span>
+            <button
+                onClick={()=> {
+                    calendarRef.current?.getInstance()?.next()
+                    syncDate()
+                }}
+            >next</button>
+
             <ToastUIReactCalendar
-                ref={calenderRef}
-                height={'700px'}
+                ref={calendarRef}
                 view={'month'}
                 calendars={calendars}
-                events={events}
+                events={coloredEvents}
                 isReadOnly={true}
-                template={template}
                 useDetailPopup={false}
                 useFormPopup={false}
+                template={template}
+                theme={theme}
                 onClickEvent={(eventInfo) => {
                     alert(`go to ${eventInfo.event.title}`)
                 }}
