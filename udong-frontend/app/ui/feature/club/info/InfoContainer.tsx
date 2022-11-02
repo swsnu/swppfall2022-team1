4e0ef32413a0cbb068d0c47f2553482ae9ebee65
yblee2001 <69137622+yblee2001@users.noreply.github.com>
2022-11-02 23:37:18 +0900

    feat: 동아리 페이지 tabs & page containers (#10)

---
+++ udong-frontend/app/ui/feature/club/info/InfoContainer.tsx
@@ -0,0 +1,7 @@
+import { VStack } from '../../../components/Stack'
+
+export const InfoContainer = () => {
+    return <VStack>
+        <h1>여기는 info</h1>
+    </VStack>
+}
---
+++ udong-frontend/app/ui/feature/club/tag/TagContainer.tsx
@@ -0,0 +1,7 @@
+import { VStack } from '../../../components/Stack'
+
+export const TagContainer = () => {
+    return <VStack>
+        <h1>여기는 tag</h1>
+    </VStack>
+}
