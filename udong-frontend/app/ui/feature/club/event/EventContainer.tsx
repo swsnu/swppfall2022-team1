13e09fa69a2dd3e7473e36766a886c1cb2c0d51a
JiyeonP <q0o0p@snu.ac.kr>
2022-11-03 05:18:28 +0900

    give padding to calendar

--- udong-frontend/app/ui/feature/club/event/EventContainer.tsx
+++ udong-frontend/app/ui/feature/club/event/EventContainer.tsx
@@ -1,4 +1,3 @@
-
 import { VStack } from '../../../components/Stack'
 import { EventCalendarView } from './calendar/EventCalendarView'
 
@@ -7,7 +6,8 @@ export const EventContainer = () => {
     return <VStack>
         <h1>Event container</h1>
         <p>hello world</p>
-
-        <EventCalendarView/>
+        <VStack paddingHorizontal={180}>
+            <EventCalendarView/>
+        </VStack>
     </VStack>
 }
