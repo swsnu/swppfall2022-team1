5d4b3f0e64ca1f4497991526da64ce6955d993c7
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-04 04:57:40 +0900

    Add comment model

--- udong-backend/udong/comment/models.py
+++ udong-backend/udong/comment/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from user.models import User
+from post.models import Post
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    # id: auto-generated
+    user_id = models.ForeignKey(
+        User, null=True, on_delete=models.SET_NULL, related_name="comment_set"
+    )
+    post_id = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comment_set"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
