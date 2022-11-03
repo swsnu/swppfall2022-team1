7a385eb633a277fdcc8d5d20de4d514c1addee65
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-03 14:33:36 +0900

    Resolve focusing out error & add module, format

--- udong-frontend/app/ui/feature/club/post/PostContentView.tsx
+++ udong-frontend/app/ui/feature/club/post/PostContentView.tsx
@@ -1,6 +1,5 @@
 import dynamic from 'next/dynamic'
-import {  Dispatch, SetStateAction, useRef }  from 'react'
-import ReactQuill from 'react-quill'
+import {  Dispatch, SetStateAction, useMemo }  from 'react'
 
 import { Spacer } from '../../../components/Spacer'
 import { VStack } from '../../../components/Stack'
@@ -15,16 +14,38 @@ interface IProps {
 
 export const PostContentView = ({ contents, setContents }: IProps) => {
 
-    const ReactQuill = dynamic(() => import('react-quill'), {
+    const ReactQuill = useMemo(() => dynamic(() => import('react-quill'), {
         ssr: false,
         loading: () => <p>Loading ...</p>,
-    })
-
-    const quill = useRef<ReactQuill>()
+    }), [])
 
     const onChange = (text: string) => {
         setContents(text)
     }
+
+    const modules = useMemo(() => ({
+        toolbar: {
+            container: [
+                [{ 'font': [] }], // font
+                [{ 'header': [1, 2, 3, 4, 5, 6, false] }], // header
+                ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block'], // tool formula: optional 
+                [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }], // list, indent 
+                ['link', 'image', 'video'], 
+                [{ 'align': [] }, { 'color': [] }, { 'background': [] }], 
+                ['clean'], // clear toolbar setting
+            ],
+        },  
+    }
+    ), [])
+
+    const formats = [
+        'font',
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block', 'formula',
+        'list', 'bullet', 'indent',
+        'link', 'image', 'video',
+        'align', 'color', 'background',        
+    ]
 
     return <VStack>
         <Spacer
@@ -34,13 +55,11 @@ export const PostContentView = ({ contents, setContents }: IProps) => {
         <h1>이 child부터가 고동현 영역</h1>
         <p>여기서 library 쓰면 됨</p>
         <ReactQuill
-            ref = {(element: ReactQuill | null) => {
-                if (element !== null) {
-                    quill.current = element
-                }
-            }}
+            modules = {modules}
+            formats = {formats}
             value = {contents}
             onChange = {onChange}
+            placeholder='내용을 입력하세요.'
             theme = {'snow'}
         />
         <p>edit에서도 재사용할 예정이니 input하는 거 외에 다른 거 X (raw한 컴포넌트)</p>
