b05dfaf58ad6b6e3ebd4c96e24b0a5412750fa97
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-04 05:07:42 +0900

    Add usertag model

--- udong-backend/udong/tag/models.py
+++ udong-backend/udong/tag/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from club.models import Club
+from user.models import User
 
 # Create your models here.
 
@@ -8,5 +9,17 @@ class Tag(models.Model):
     # id: auto-generated
     club_id = models.ForeignKey(Club, on_delete=models.CASCADE, related_name="tag_set")
     name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class UserTag(models.Model):
+    # id: auto-generated
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_tag_set"
+    )
+    tag_id = models.ForeignKey(
+        Tag, on_delete=models.CASCADE, related_name="tag_user_set"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
