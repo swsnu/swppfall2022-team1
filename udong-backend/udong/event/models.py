bf17b40cb6c5fbff861b5ba78696ce849fe6ac90
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-04 02:09:42 +0900

    Add event model

--- udong-backend/udong/event/models.py
+++ udong-backend/udong/event/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from club.models import Club
 
 # Create your models here.
+
+
+class Event(models.Model):
+    # id: auto-generated
+    club_id = models.ForeignKey(
+        Club, on_delete=models.CASCADE, related_name="event_set"
+    )
+    name = models.CharField(max_length=255, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
