599b2d65fd8927ef6313ed2ce103b66d411f7bb9
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-04 05:02:51 +0900

    Add tag model

--- udong-backend/udong/tag/models.py
+++ udong-backend/udong/tag/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from club.models import Club
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    # id: auto-generated
+    club_id = models.ForeignKey(Club, on_delete=models.CASCADE, related_name="tag_set")
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
