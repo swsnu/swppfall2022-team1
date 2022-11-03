482e5dbb8721f82fe9f5b1abe089ec6c0650ebc6
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-04 00:46:25 +0900

    Add user model

--- udong-backend/udong/user/models.py
+++ udong-backend/udong/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    # id: auto-generated
+    google = models.TextField()
+    image = models.TextField()
+    time_table = models.TextField()
+    name = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
