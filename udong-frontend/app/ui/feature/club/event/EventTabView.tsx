bc710b70a33eb568a164d057431c3a88b1bf81c7
JiyeonP <q0o0p@snu.ac.kr>
2022-11-04 16:12:40 +0900

    make event tab

--- udong-frontend/app/ui/feature/club/event/EventTabView.tsx
+++ udong-frontend/app/ui/feature/club/event/EventTabView.tsx
@@ -1,6 +1,13 @@
 import { useCallback } from 'react'
 
-import { HStack } from '../../../components/Stack'
+import { Spacer } from '../../../components/Spacer'
+import { HStack, VStack } from '../../../components/Stack'
+import UdongSelectableIcon from '../../../components/UdongSelectableIcon'
+import IcCalendarOff from '../../../icons/IcCalendarOff.png'
+import IcCalendarOn from '../../../icons/IcCalendarOn.png'
+import IcListOff from '../../../icons/IcListOff.png'
+import IcListOn from '../../../icons/IcListOn.png'
+import { UdongColors } from '../../../theme/ColorPalette'
 
 export type EventTabType = EVENT_TAB
 
@@ -17,18 +24,63 @@ interface EventTabViewProps {
 export const EventTabView = (props: EventTabViewProps) => {
     const { selectedTab, setSelectedTab } = props
 
-    const handleOnViewClick = useCallback((view: EventTabType) => {
-        setSelectedTab(view)
+    const handleOnViewClick = useCallback((tab: EventTabType) => {
+        setSelectedTab(tab)
     }, [setSelectedTab])
 
-    return <HStack height={35}>
-        <button
-            disabled={selectedTab === EVENT_TAB.CALENDAR}
-            onClick={() => handleOnViewClick(EVENT_TAB.CALENDAR)}
-        >calendar</button>
-        <button
-            disabled={selectedTab === EVENT_TAB.LIST}
-            onClick={() => handleOnViewClick(EVENT_TAB.LIST)}
-        >list</button>
+    return <HStack
+        height={30}
+        gap={10}
+    >
+        <VStack
+            width={35}
+            justifyContent={'space-between'}
+        >
+            <HStack
+                width={35}
+                justifyContent={'center'}
+            >
+                <UdongSelectableIcon
+                    selected={selectedTab === EVENT_TAB.CALENDAR}
+                    selectedIcon={IcCalendarOn}
+                    unselectedIcon={IcCalendarOff}
+                    onClickUnSelected={()=>handleOnViewClick(EVENT_TAB.CALENDAR)}
+                />
+            </HStack>
+            {selectedTab === EVENT_TAB.CALENDAR ?
+                <Spacer
+                    height={2}
+                    backgroundColor={UdongColors.Primary}
+                /> : <Spacer
+                    height={1}
+                    backgroundColor={UdongColors.GrayBright}
+                />
+            }
+        </VStack>
+        <VStack
+            width={35}
+            justifyContent={'space-between'}
+        >
+            <HStack
+                width={35}
+                justifyContent={'center'}
+            >
+                <UdongSelectableIcon
+                    selected={selectedTab === EVENT_TAB.LIST}
+                    selectedIcon={IcListOn}
+                    unselectedIcon={IcListOff}
+                    onClickUnSelected={()=>handleOnViewClick(EVENT_TAB.LIST)}
+                />
+            </HStack>
+            {selectedTab === EVENT_TAB.LIST ?
+                <Spacer
+                    height={2}
+                    backgroundColor={UdongColors.Primary}
+                /> : <Spacer
+                    height={1}
+                    backgroundColor={UdongColors.GrayBright}
+                />
+            }
+        </VStack>
     </HStack>
 }
