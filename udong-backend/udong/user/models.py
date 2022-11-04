33a5cc74756f65c03c75a73fdbb79ca0243f0498
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-04 17:01:03 +0900

    Fix user name's max_length to 255

--- udong-backend/udong/user/models.py
+++ udong-backend/udong/user/models.py
@@ -9,7 +9,7 @@ class User(models.Model):
     google = models.TextField()
     image = models.TextField()
     time_table = models.TextField()
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
