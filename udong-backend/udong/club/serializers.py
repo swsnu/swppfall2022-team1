ed7a987c559ac6e1abb53a0a4bb39de130ff2810
Donghyun Koh <33983582+kohandy1902@users.noreply.github.com>
2022-11-06 05:57:17 +0900

    :clubs: Back/feat/get club (#48)

--- udong-backend/udong/club/serializers.py
+++ udong-backend/udong/club/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
+from rest_framework.utils.serializer_helpers import ReturnDict
 from club.models import Club
+from user.models import UserClub
+from event.models import Event
+from tag.models import Tag
+from user.serializers import UserSerializer
+from event.serializers import EventTimeSerializer
 
 
 class ClubSerializer(serializers.ModelSerializer[Club]):
@@ -13,6 +19,55 @@ class ClubSerializer(serializers.ModelSerializer[Club]):
             "id",
             "name",
             "code",
+            "created_at",
+            "updated_at",
+        )
+
+
+class ClubUserSerializer(serializers.ModelSerializer[UserClub]):
+    user = UserSerializer()
+    auth = serializers.CharField(source="get_auth_display")
+    created_at = serializers.DateTimeField(required=False)
+    updated_at = serializers.DateTimeField(required=False)
+
+    class Meta:
+        model = UserClub
+        fields = (
+            "user",
+            "auth",
+            "created_at",
+            "updated_at",
+        )
+
+
+class ClubEventSerializer(serializers.ModelSerializer[Event]):
+    name = serializers.CharField(max_length=255)
+    time = serializers.SerializerMethodField(read_only=True)
+    created_at = serializers.DateTimeField(required=False)
+    updated_at = serializers.DateTimeField(required=False)
+
+    class Meta:
+        model = Event
+        fields = (
+            "name",
+            "time",
+            "created_at",
+            "updated_at",
+        )
+
+    def get_time(self, event: Event) -> ReturnDict:
+        return EventTimeSerializer(event.time_set, many=True, context=self.context).data
+
+
+class ClubTagSerializer(serializers.ModelSerializer[Tag]):
+    name = serializers.CharField(max_length=255)
+    created_at = serializers.DateTimeField(required=False)
+    updated_at = serializers.DateTimeField(required=False)
+
+    class Meta:
+        model = Tag
+        fields = (
+            "name",
             "created_at",
             "updated_at",
         )
--- udong-backend/udong/club/views.py
+++ udong-backend/udong/club/views.py
@@ -1,17 +1,66 @@
 from django.shortcuts import render
+from django.db.models import Model
 from rest_framework import viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.serializers import BaseSerializer
 from club.models import Club
-from club.serializers import ClubSerializer
+from club.serializers import (
+    ClubSerializer,
+    ClubUserSerializer,
+    ClubEventSerializer,
+    ClubTagSerializer,
+)
+from typing import Any, Type, TypeVar
 
 # Create your views here.
+
+_MT_co = TypeVar("_MT_co", bound=Model, covariant=True)
 
 
 class ClubViewSet(viewsets.GenericViewSet):
     queryset = Club.objects.all()
     serializer_class = ClubSerializer
 
+    def get_serializer_class(self) -> Type[BaseSerializer[_MT_co]]:
+        if self.action == "user":
+            return ClubUserSerializer
+        if self.action == "event":
+            return ClubEventSerializer
+        if self.action == "tag":
+            return ClubTagSerializer
+        return ClubSerializer
+
     def list(self, request: Request) -> Response:
         club = self.get_queryset().filter(club_user_set__user__id=request.user.id)
         return Response(self.get_serializer(club, many=True).data)
+
+    def retrieve(self, request: Request, pk: Any = None) -> Response:
+        club = self.get_object()
+        return Response(self.get_serializer(club).data)
+
+    @action(detail=True, methods=["GET"])
+    def user(self, request: Request, pk: Any) -> Response:
+        club_user = (
+            self.get_queryset()
+            .prefetch_related("club_user_set__user")
+            .get(id=pk)
+            .club_user_set
+        )
+        return Response(self.get_serializer(club_user, many=True).data)
+
+    @action(detail=True, methods=["GET"])
+    def event(self, request: Request, pk: Any) -> Response:
+        club_event = (
+            self.get_queryset()
+            .prefetch_related("event_set__time_set")
+            .get(id=pk)
+            .event_set
+        )
+        return Response(self.get_serializer(club_event, many=True).data)
+
+    @action(detail=True, methods=["GET"])
+    def tag(self, request: Request, pk: Any) -> Response:
+        club_tag = self.get_object().tag_set
+        return Response(self.get_serializer(club_tag, many=True).data)
