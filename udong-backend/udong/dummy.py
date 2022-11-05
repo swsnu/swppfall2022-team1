7d8b91099c3bf692eca6656561bd3b243af58dde
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-06 02:41:42 +0900

    Make register dummy

--- udong-backend/udong/dummy.py
+++ udong-backend/udong/dummy.py
@@ -1,9 +1,18 @@
 import os
-from user.models import User
+from user.models import User, UserClub
+from club.models import Club
 
 # Refresh DB
 os.system("rm -f db.sqlite3")
 os.system("make migrate")
 
 # Create dummy user
-User.objects.create(name="Alan Turing")
+user1 = User.objects.create(name="Alan Turing")
+
+# Create dummy club
+club1 = Club.objects.create(name="Udong", code="swppfall")
+club2 = Club.objects.create(name="Ramen", code="random")
+
+# Register club
+UserClub.objects.create(user=user1, club=club1, auth="A")
+UserClub.objects.create(user=user1, club=club2, auth="M")
