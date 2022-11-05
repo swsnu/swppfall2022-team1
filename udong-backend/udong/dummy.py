eb3b59ba05eeb53d452fbaaa24816930b6431b72
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-06 04:48:52 +0900

    Add dummy event and its time

--- udong-backend/udong/dummy.py
+++ udong-backend/udong/dummy.py
@@ -1,6 +1,9 @@
 import os
 from user.models import User, UserClub
 from club.models import Club
+from event.models import Event
+from timedata.models import Time
+from datetime import datetime
 
 # Refresh DB
 os.system("rm -f db.sqlite3")
@@ -21,3 +24,15 @@ club2 = Club.objects.create(name="Ramen", code="random")
 UserClub.objects.create(user=user1, club=club1, auth="A")
 UserClub.objects.create(user=user2, club=club1, auth="M")
 UserClub.objects.create(user=user1, club=club2, auth="M")
+
+# Create dummy event
+event1 = Event.objects.create(club=club1, name="Turing award")
+event2 = Event.objects.create(club=club1, name="Nobel Prize")
+
+# Create dummy time
+time1 = Time.objects.create(
+    event=event1,
+    type="D",
+    start_date=datetime(2022, 11, 6),
+    end_date=datetime(2022, 11, 7),
+)
