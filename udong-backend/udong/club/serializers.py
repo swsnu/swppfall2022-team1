0708dbf28912e985f04c1368a3212503bd0a974a
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-06 04:15:37 +0900

    Add ClubUserSerializer

--- udong-backend/udong/club/serializers.py
+++ udong-backend/udong/club/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from club.models import Club
+from user.models import UserClub
+from user.serializers import UserSerializer
 
 
 class ClubSerializer(serializers.ModelSerializer[Club]):
@@ -13,6 +15,22 @@ class ClubSerializer(serializers.ModelSerializer[Club]):
             "id",
             "name",
             "code",
+            "created_at",
+            "updated_at",
+        )
+
+
+class ClubUserSerializer(serializers.ModelSerializer[UserClub]):
+    user = UserSerializer()
+    auth = serializers.CharField(source="get_auth_display")
+    created_at = serializers.DateTimeField(required=False)
+    updated_at = serializers.DateTimeField(required=False)
+
+    class Meta:
+        model = UserClub
+        fields = (
+            "user",
+            "auth",
             "created_at",
             "updated_at",
         )
