8a5efdd5c57c0905502748dd9b75fbe4cb17ca36
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-05 21:43:48 +0900

    Set user api url

--- udong-backend/udong/udong/urls.py
+++ udong-backend/udong/udong/urls.py
@@ -38,6 +38,7 @@ schema_view = get_schema_view(
 
 urlpatterns: List[Union[URLPattern, URLResolver]] = [
     path("admin/", admin.site.urls),
+    path("api/", include("user.urls")),
 ]
 
 if settings.DEBUG:
