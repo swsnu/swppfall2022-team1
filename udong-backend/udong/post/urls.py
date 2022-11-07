2122dc9b439536538314e66eab174e94a07f6a2c
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-08 02:37:33 +0900

    Fix urls.py

--- udong-backend/udong/post/urls.py
+++ udong-backend/udong/post/urls.py
@@ -1,9 +1,9 @@
 from rest_framework.routers import SimpleRouter
-from post.views import PostViewSet
+from post.views import PostClubViewSet
 
 app_name = "post"
 
 router = SimpleRouter()
-router.register("post", PostViewSet)
+router.register("post/club", PostClubViewSet)
 
 urlpatterns = router.urls
