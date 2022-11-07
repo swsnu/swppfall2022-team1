684dc849dfe57d41626fd7613893126f25401bc1
JiyeonP <q0o0p@snu.ac.kr>
2022-11-08 01:36:05 +0900

    remove fake loading part

--- udong-frontend/app/ui/feature/club/board/BoardContainer.tsx
+++ udong-frontend/app/ui/feature/club/board/BoardContainer.tsx
@@ -1,21 +1,13 @@
 import { useRouter } from 'next/router'
-import { useState } from 'react'
 
 import { Spacer } from '../../../components/Spacer'
 import { HStack, VStack } from '../../../components/Stack'
 import { UdongButton } from '../../../components/UdongButton'
-import UdongLoader from '../../../components/UdongLoader'
 import { UdongSearchBar } from '../../../components/UdongSearchBar'
 import { PostItem } from '../../shared/PostItem'
 import { ScrollToTopButton } from '../../shared/ScrollToTopButton'
 
-const delay = (time: number) => {
-    return new Promise(resolve => setTimeout(resolve, time))
-}
-
 export const BoardContainer = () => {
-    const [loading, setLoading] = useState(true)
-    delay(600).then(() => setLoading(false))
 
     const router = useRouter()
     return <VStack>
@@ -33,19 +25,15 @@ export const BoardContainer = () => {
         <UdongSearchBar/>
         <Spacer height={8}/>
 
-        {loading ? <UdongLoader height={400}/> :
-            <VStack>
-                <PostItem isClubBoard={true}/>
-                <PostItem isClubBoard={true}/>
-                <PostItem isClubBoard={true}/>
-                <PostItem isClubBoard={true}/>
-                {/* 코드 중복 잘 것 같아서 주석 달기*/}
-                <PostItem isClubBoard={true}/>
-                <PostItem isClubBoard={true}/>
-                <PostItem isClubBoard={true}/>
-                <PostItem isClubBoard={true}/>
-                <ScrollToTopButton/>
-            </VStack>
-        }
+        <PostItem isClubBoard={true}/>
+        <PostItem isClubBoard={true}/>
+        <PostItem isClubBoard={true}/>
+        <PostItem isClubBoard={true}/>
+        {/* 코드 중복 잘 것 같아서 주석 달기*/}
+        <PostItem isClubBoard={true}/>
+        <PostItem isClubBoard={true}/>
+        <PostItem isClubBoard={true}/>
+        <PostItem isClubBoard={true}/>
+        <ScrollToTopButton/>
     </VStack>
 }
