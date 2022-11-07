eb957723881e854e40e520ad45aea2c05123059c
JiyeonP <q0o0p@snu.ac.kr>
2022-11-08 01:11:36 +0900

    set fake loading time to feed, board

--- udong-frontend/app/ui/feature/club/board/BoardContainer.tsx
+++ udong-frontend/app/ui/feature/club/board/BoardContainer.tsx
@@ -1,13 +1,22 @@
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 import { Spacer } from '../../../components/Spacer'
 import { HStack, VStack } from '../../../components/Stack'
 import { UdongButton } from '../../../components/UdongButton'
+import UdongLoader from '../../../components/UdongLoader'
 import { UdongSearchBar } from '../../../components/UdongSearchBar'
 import { PostItem } from '../../shared/PostItem'
 import { ScrollToTopButton } from '../../shared/ScrollToTopButton'
 
+const delay = (time: number) => {
+    return new Promise(resolve => setTimeout(resolve, time))
+}
+
 export const BoardContainer = () => {
+    const [loading, setLoading] = useState(true)
+    delay(600).then(() => setLoading(false))
+
     const router = useRouter()
     return <VStack>
         <HStack justifyContent={'end'}>
@@ -24,15 +33,19 @@ export const BoardContainer = () => {
         <UdongSearchBar/>
         <Spacer height={8}/>
 
-        <PostItem isClubBoard={true}/>
-        <PostItem isClubBoard={true}/>
-        <PostItem isClubBoard={true}/>
-        <PostItem isClubBoard={true}/>
-        {/* 코드 중복 잘 것 같아서 주석 달기*/}
-        <PostItem isClubBoard={true}/>
-        <PostItem isClubBoard={true}/>
-        <PostItem isClubBoard={true}/>
-        <PostItem isClubBoard={true}/>
-        <ScrollToTopButton/>
+        {loading ? <UdongLoader height={400}/> :
+            <VStack>
+                <PostItem isClubBoard={true}/>
+                <PostItem isClubBoard={true}/>
+                <PostItem isClubBoard={true}/>
+                <PostItem isClubBoard={true}/>
+                {/* 코드 중복 잘 것 같아서 주석 달기*/}
+                <PostItem isClubBoard={true}/>
+                <PostItem isClubBoard={true}/>
+                <PostItem isClubBoard={true}/>
+                <PostItem isClubBoard={true}/>
+                <ScrollToTopButton/>
+            </VStack>
+        }
     </VStack>
 }
--- udong-frontend/app/ui/feature/home/feed/FeedContainer.tsx
+++ udong-frontend/app/ui/feature/home/feed/FeedContainer.tsx
@@ -1,25 +1,40 @@
+import { useState } from 'react'
+
 import { Spacer } from '../../../components/Spacer'
 import { VStack } from '../../../components/Stack'
+import UdongLoader from '../../../components/UdongLoader'
 import { UdongSearchBar } from '../../../components/UdongSearchBar'
 import { PostItem } from '../../shared/PostItem'
 import { ScrollToTopButton } from '../../shared/ScrollToTopButton'
 
+const delay = (time: number) => {
+    return new Promise(resolve => setTimeout(resolve, time))
+}
+
 export const FeedContainer = () => {
+    const [loading, setLoading] = useState(true)
+    delay(600).then(() => setLoading(false))
+
     return <VStack>
         <UdongSearchBar/>
         <Spacer height={8}/>
 
         <ScrollToTopButton/>
 
-        <PostItem/>
-        <PostItem/>
-        <PostItem/>
-        <PostItem/>
-        <PostItem/>
-        <PostItem/>
-        <PostItem/>
-        <PostItem/>
-        <PostItem/>
-        <PostItem/>
+        {loading ? <UdongLoader height={500}/> :
+            <VStack>
+                <PostItem/>
+                <PostItem/>
+                <PostItem/>
+                <PostItem/>
+                <PostItem/>
+                <PostItem/>
+                <PostItem/>
+                <PostItem/>
+                <PostItem/>
+                <PostItem/>
+                <ScrollToTopButton/>
+            </VStack>
+        }
     </VStack>
 }
