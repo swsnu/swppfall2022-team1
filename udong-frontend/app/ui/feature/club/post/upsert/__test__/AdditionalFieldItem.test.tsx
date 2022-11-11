73c96aa96615e4db6d326553a521f94b6663db32
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-12 03:34:24 +0900

    Add expect

--- udong-frontend/app/ui/feature/club/post/upsert/__test__/AdditionalFieldItem.test.tsx
+++ udong-frontend/app/ui/feature/club/post/upsert/__test__/AdditionalFieldItem.test.tsx
@@ -1,9 +1,10 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import { AdditionalFieldItem } from '../AdditionalFieldItem'
 
 describe('<AdditionalFieldItem />', () => {
     it('should render', () => {
         render(<AdditionalFieldItem item={'test'} />)
+        expect(screen.getByText('test')).toBeInTheDocument()
     })
 })
