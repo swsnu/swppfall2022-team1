a51be7ddc02e7fe1f4f54a6362ebda6463b3cee5
Donghyun Koh <33983582+kohandy1902@users.noreply.github.com>
2022-11-12 01:22:24 +0900

    :clubs: Back/test/club (#107)

--- udong-backend/udong/club/tests.py
+++ udong-backend/udong/club/tests.py
@@ -1,3 +1,136 @@
-from django.test import TestCase
+from django.test import Client
+from common.utils import MyTestCase
+from user.models import UserClub
+from club.models import Club
+from event.models import Event
+from timedata.models import Time
+from tag.models import Tag
+from datetime import date
 
 # Create your tests here.
+
+
+class ClubTestCase(MyTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        club1 = Club.objects.create(name="Udong", code="swppfall")
+        club2 = Club.objects.create(name="Ramen", code="random")
+        UserClub.objects.create(user=self.dummy_user, club=club1, auth="A")
+        event1 = Event.objects.create(club=club1, name="Turing award")
+        Time.objects.create(
+            event=event1,
+            type="D",
+            start_date=date(2022, 11, 6),
+            end_date=date(2022, 11, 7),
+            start_time=10,
+            end_time=30,
+        )
+        Time.objects.create(
+            event=event1,
+            type="W",
+            repeat_start=date(2022, 11, 6),
+            repeat_end=date(2022, 11, 7),
+            weekday=5,
+            start_time=15,
+            end_time=35,
+        )
+        Tag.objects.create(club=club1, name="genius")
+        Tag.objects.create(club=club1, name="winner")
+
+    # api/club/:id
+    def test_club_id(self) -> None:
+        client = Client()
+
+        response = client.get("/api/club/1/")
+        self.assertEqual(response.status_code, 200)
+        self.jsonEqual(response.content, {"id": 1, "name": "Udong", "code": "swppfall"})
+
+    # api/club/
+    def test_club_list(self) -> None:
+        client = Client()
+
+        response = client.get("/api/club/")
+        self.assertEqual(response.status_code, 200)
+        self.jsonEqual(
+            response.content, [{"id": 1, "name": "Udong", "code": "swppfall"}]
+        )
+
+    # api/club/:id/user/
+    def test_club_user(self) -> None:
+        client = Client()
+
+        response = client.get("/api/club/1/user/")
+        self.assertEqual(response.status_code, 200)
+        self.jsonEqual(
+            response.content,
+            [
+                {
+                    "user": {
+                        "id": 1,
+                        "google": "google",
+                        "image": "image",
+                        "time_table": "001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011",
+                        "name": "Alan Turing",
+                    },
+                    "auth": "Admin",
+                }
+            ],
+        )
+
+    # api/club/:id/event/
+    def test_club_event(self) -> None:
+        client = Client()
+
+        response = client.get("/api/club/1/event/")
+        self.assertEqual(response.status_code, 200)
+        self.jsonEqual(
+            response.content,
+            [
+                {
+                    "id": 1,
+                    "name": "Turing award",
+                    "time": [
+                        {
+                            "type": "Date",
+                            "start_date": "2022-11-06",
+                            "end_date": "2022-11-07",
+                            "repeat_start": None,
+                            "repeat_end": None,
+                            "weekday": None,
+                            "start_time": 10,
+                            "end_time": 30,
+                        },
+                        {
+                            "type": "Weekday",
+                            "start_date": None,
+                            "end_date": None,
+                            "repeat_start": "2022-11-06",
+                            "repeat_end": "2022-11-07",
+                            "weekday": 5,
+                            "start_time": 15,
+                            "end_time": 35,
+                        },
+                    ],
+                },
+            ],
+        )
+
+    # api/club/:id/tag/
+    def test_club_tag(self) -> None:
+        client = Client()
+
+        response = client.get("/api/club/1/tag/")
+        self.assertEqual(response.status_code, 200)
+        self.jsonEqual(
+            response.content,
+            [
+                {
+                    "id": 1,
+                    "name": "genius",
+                },
+                {
+                    "id": 2,
+                    "name": "winner",
+                },
+            ],
+        )
--- udong-backend/udong/common/utils.py
+++ udong-backend/udong/common/utils.py
@@ -1,9 +1,61 @@
 from django.test import TestCase
+from user.models import User
 from typing import Dict, List
 import json
 
 
-JsonType = int | str | List["JsonType"] | Dict[str, "JsonType"]
+JsonType = List["JsonType"] | Dict[str, "JsonType"] | int | str | None
+
+
+class MyJson:
+    def __init__(self, json: JsonType) -> None:
+        self.json = json
+
+    # We don't have to check created_at & updated_at
+    @staticmethod
+    def remove_auto_generated_field(json: JsonType) -> None:
+        if isinstance(json, dict):
+            for key in ("created_at", "updated_at"):
+                if key in json:
+                    del json[key]
+            for value in json.values():
+                MyJson.remove_auto_generated_field(value)
+
+        elif isinstance(json, list):
+            for dictionary in json:
+                MyJson.remove_auto_generated_field(dictionary)
+
+    @staticmethod
+    def compare(json1: JsonType, json2: JsonType) -> bool:
+        if isinstance(json1, list) and isinstance(json2, list):
+            if len(json1) != len(json2):
+                return False
+            for i in range(len(json1)):
+                if MyJson.compare(json1[i], json2[i]) == False:
+                    return False
+                return True
+        elif isinstance(json1, dict) and isinstance(json2, dict):
+            if sorted(json1) != sorted(json2):
+                return False
+            for key in json1.keys():
+                if MyJson.compare(json1[key], json2[key]) == False:
+                    return False
+                return True
+        elif isinstance(json1, int) and isinstance(json2, int):
+            return json1 == json2
+        elif isinstance(json1, str) and isinstance(json2, str):
+            return json1 == json2
+        elif json1 is None and json2 is None:
+            return True
+        else:
+            return False
+        return False
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, MyJson):
+            return MyJson.compare(self.json, other.json)
+        else:
+            return False
 
 
 class MyTestCase(TestCase):
@@ -14,12 +66,17 @@ class MyTestCase(TestCase):
     # response = client.get(...)
     # jsonEqual(response.content, {"id": 1})
     def jsonEqual(self, j1: bytes, j2: JsonType) -> None:
-        json_j1 = json.loads(j1)
-        json_j2 = json.loads(json.dumps(j2))
+        json_j1 = MyJson(json.loads(j1))
+        json_j2 = MyJson(json.loads(json.dumps(j2)))
 
-        # We don't have to check created_at & updated_at
-        for key in ("created_at", "updated_at"):
-            if key in json_j1:
-                del json_j1[key]
+        MyJson.remove_auto_generated_field(json_j1.json)
+        self.assertEqual(json_j1, json_j2)
 
-        self.assertEqual(sorted(json_j1), sorted(json_j2))
+    # Add Dummy User
+    def setUp(self) -> None:
+        self.dummy_user = User.objects.create(
+            name="Alan Turing",
+            google="google",
+            image="image",
+            time_table="001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011001101100110110011011",
+        )
