7c922f582fec64e796df3c86669d14e35144eeb9
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-11 22:47:07 +0900

    Test api/club/

--- udong-backend/udong/club/tests.py
+++ udong-backend/udong/club/tests.py
@@ -1,5 +1,6 @@
 from django.test import Client
 from common.utils import MyTestCase
+from user.models import UserClub
 from club.models import Club
 
 # Create your tests here.
@@ -8,8 +9,9 @@ from club.models import Club
 class ClubTestCase(MyTestCase):
     def setUp(self) -> None:
         super().setUp()
-        Club.objects.create(name="Udong", code="swppfall")
-        Club.objects.create(name="Ramen", code="random")
+        club1 = Club.objects.create(name="Udong", code="swppfall")
+        club2 = Club.objects.create(name="Ramen", code="random")
+        UserClub.objects.create(user=self.dummy_user, club=club1, auth="A")
 
     # api/club/:id
     def test_club_id(self) -> None:
@@ -18,3 +20,13 @@ class ClubTestCase(MyTestCase):
         response = client.get("/api/club/1/")
         self.assertEqual(response.status_code, 200)
         self.jsonEqual(response.content, {"id": 1, "name": "Udong", "code": "swppfall"})
+
+    # api/club/
+    def test_club_list(self) -> None:
+        client = Client()
+
+        response = client.get("/api/club/")
+        self.assertEqual(response.status_code, 200)
+        self.jsonEqual(
+            response.content, [{"id": 1, "name": "Udong", "code": "swppfall"}]
+        )
