47fc4a4a5812161901c5bb8c80e07c16f8333030
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-11 21:07:05 +0900

    Remove created_at & updated_at field in compare

--- udong-backend/udong/common/utils.py
+++ udong-backend/udong/common/utils.py
@@ -3,11 +3,17 @@ from typing import Dict, List
 import json
 
 
-JsonType = str | List["JsonType"] | Dict[str, "JsonType"]
+JsonType = int | str | List["JsonType"] | Dict[str, "JsonType"]
 
 
 class MyTestCase(TestCase):
     def jsonEqual(self, j1: bytes, j2: JsonType) -> None:
         json_j1 = json.loads(j1)
         json_j2 = json.loads(json.dumps(j2))
+
+        # We don't have to check created_at & updated_at
+        for key in ("created_at", "updated_at"):
+            if key in json_j1:
+                del json_j1[key]
+
         self.assertEqual(sorted(json_j1), sorted(json_j2))
