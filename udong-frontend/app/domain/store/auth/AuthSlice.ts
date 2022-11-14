2e17a528baf208462bd8f825472f8f39468f4943
yblee2001 <yblee2001@gmail.com>
2022-11-14 12:41:41 +0900

    feat: auth slice & selector

---
+++ udong-frontend/app/domain/store/auth/AuthSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+interface AuthState {
+    // TODO: 나중에 바꾸기
+    isLoggedIn: boolean
+}
+
+const initialState: AuthState = {
+    isLoggedIn: false,
+}
+
+// TODO: actions
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {},
+})
+
+export const authActions = authSlice.actions
+export const authReducer = authSlice.reducer
--- udong-frontend/app/domain/store/index.ts
+++ udong-frontend/app/domain/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import { authReducer } from './auth/AuthSlice'
 import { clubReducer } from './club/ClubSlice'
 
 export const store = configureStore({
     reducer: {
+        auth: authReducer,
         club: clubReducer,
     },
 })
