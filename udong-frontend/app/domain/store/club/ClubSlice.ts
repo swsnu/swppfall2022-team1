5d33ece869bcfe47fa93e197cc8993135e36e8d8
yblee2001 <yblee2001@gmail.com>
2022-11-14 18:12:13 +0900

    feat: define ClubSlice actions

--- udong-frontend/app/domain/store/club/ClubSlice.ts
+++ udong-frontend/app/domain/store/club/ClubSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 import { Club } from '../../model/Club'
 
@@ -9,23 +9,61 @@ interface ClubState {
 const initialState: ClubState = {
 }
 
-// TODO: actions
-//        getMyClubs,
-//         getClub,
-//         registerClub,
-//         createClub,
-//         editClub,
-//         leaveClub,
-//         deleteClub,
-//         getClubMembers,
-//         removeClubMember,
-//         assignClubMemberRole,
-
 const clubSlice = createSlice({
     name: 'club',
     initialState,
     reducers: {},
 })
+
+export const getMyClubs = createAsyncThunk(
+    'club/getMyClubs',
+    async () => { return },
+)
+
+export const getClub = createAsyncThunk(
+    'club/getClub',
+    async () => { return },
+)
+
+export const registerClub = createAsyncThunk(
+    'club/registerClub',
+    async () => { return },
+)
+
+export const createClub = createAsyncThunk(
+    'club/createClub',
+    async () => { return },
+)
+
+export const editClub = createAsyncThunk(
+    'club/editClub',
+    async () => { return },
+)
+
+export const leaveClub = createAsyncThunk(
+    'club/leaveClub',
+    async () => { return },
+)
+
+export const deleteClub = createAsyncThunk(
+    'club/deleteClub',
+    async () => { return },
+)
+
+export const getClubMembers = createAsyncThunk(
+    'club/getClubMembers',
+    async () => { return },
+)
+
+export const removeClubMember = createAsyncThunk(
+    'club/removeClubMember',
+    async () => { return },
+)
+
+export const assignClubMemberRole = createAsyncThunk(
+    'club/assignClubMemberRole',
+    async () => { return },
+)
 
 export const clubActions = clubSlice.actions
 export const clubReducer = clubSlice.reducer
