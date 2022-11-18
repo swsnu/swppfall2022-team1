bdb18997229f31b9eec51bae51897430789ac8dd
yblee2001 <yblee2001@gmail.com>
2022-11-19 03:58:10 +0900

    test: fix tests

--- udong-frontend/app/ui/feature/club/info/__test__/InfoContainer.test.tsx
+++ udong-frontend/app/ui/feature/club/info/__test__/InfoContainer.test.tsx
@@ -1,22 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { render, screen } from '@testing-library/react'
+import * as router from 'next/router'
+import { NextRouter } from 'next/router'
 import { Provider } from 'react-redux'
 
 import { dummyUserMe } from '../../../../../domain/model/User'
+import { clubReducer, ClubState } from '../../../../../domain/store/club/ClubSlice'
 import { userReducer, UserState } from '../../../../../domain/store/user/UserSlice'
 import { InfoContainer } from '../InfoContainer'
 
-const stubInitialState: UserState = {
+const stubUserInitialState: UserState = {
     selectedUser: dummyUserMe,
 }
 
+const stubClubInitialState: ClubState = {
+    myClubs: [],
+}
+
 const mockStore = configureStore({
-    reducer: { user: userReducer },
-    preloadedState: { user: stubInitialState },
+    reducer: { user: userReducer, club: clubReducer },
+    preloadedState: { user: stubUserInitialState, club: stubClubInitialState },
 })
 
 describe('<InfoContainer/>', () => {
     it('should render info container', () => {
+        jest.spyOn(router, 'useRouter').mockImplementation(() => ({
+            query: { clubId: 1 },
+        } as unknown as NextRouter))
+
         render(<Provider store={mockStore}>
             <InfoContainer/>
         </Provider>,
