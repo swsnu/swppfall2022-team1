50a1b0cc385276887d3e8ffe277125869f916721
yblee2001 <yblee2001@gmail.com>
2022-11-19 15:24:00 +0900

    fix: apply changes requested

--- udong-frontend/app/infra/transformer/PostTagTransformer.ts
+++ udong-frontend/app/infra/transformer/PostTagTransformer.ts
@@ -2,7 +2,10 @@ import { PostTag } from '../../domain/model/Tag'
 import { PostTagDto } from '../dto/PostTagDto'
 
 const fromDto = (dto: PostTagDto): PostTag => {
-    return dto
+    return {
+        id: dto.id,
+        name: dto.name,
+    }
 }
 
 export const postTagTransformer = {
