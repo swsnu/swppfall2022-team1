d322a8fc01586887c6402741cc3a9c5872f9edbf
Yujin Im <69783927+gina0605@users.noreply.github.com>
2022-11-19 23:07:53 +0900

    :sparkles: Implement POST, PUT DELETE comment (#144)

--- udong-backend/udong/comment/views.py
+++ udong-backend/udong/comment/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from typing import Any
+from django.core.exceptions import PermissionDenied
+from rest_framework import viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
+from comment.models import Comment
+from comment.serializers import CommentSerializer
 
 # Create your views here.
+
+
+class CommentViewSet(viewsets.GenericViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def update(self, request: Request, pk: Any = None) -> Response:
+        user = request.user
+        comment = self.get_object()
+        if comment.user_id != user.id:
+            raise PermissionDenied()
+        serializer = self.get_serializer(
+            comment,
+            data={"content": request.data.get("content")},
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.update(comment, serializer.validated_data)
+        return Response(serializer.data)
+
+    def destroy(self, request: Request, pk: Any = None) -> Response:
+        user = request.user
+        comment = self.get_object()
+        if comment.user_id != user.id:
+            raise PermissionDenied()
+        comment.delete()
+        return Response()
--- udong-backend/udong/post/views.py
+++ udong-backend/udong/post/views.py
@@ -1,14 +1,44 @@
-from django.shortcuts import render
 from django.db.models import Q
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from post.models import Post
 from post.serializers import PostBoardSerializer
 from user.models import UserClub
+from comment.serializers import CommentSerializer
 from typing import Any
 
 # Create your views here.
+
+
+class PostViewSet(viewsets.GenericViewSet):
+    queryset = Post.objects.all()
+    serializer_class = CommentSerializer
+
+    @action(detail=True, methods=["GET", "POST"])
+    def comment(self, request: Request, pk: Any) -> Response:
+        if request.method == "GET":
+            return self._get_comments(request, pk)
+        elif request.method == "POST":
+            return self._post_comment(request, pk)
+        else:
+            raise MethodNotAllowed(
+                request.method if request.method else "unknown method"
+            )
+
+    def _get_comments(self, request: Request, pk: Any) -> Response:
+        # TODO: implement GET /post/:id/comment/
+        return Response()
+
+    def _post_comment(self, request: Request, pk: Any) -> Response:
+        serializer = self.get_serializer(
+            data=request.data, context={"post_id": pk, "user": request.user}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class PostClubViewSet(viewsets.GenericViewSet):
