743faf91fb9fa7047e5f618942325a73f60e8c92
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-20 03:38:10 +0900

    Add post/ api

--- udong-backend/udong/post/views.py
+++ udong-backend/udong/post/views.py
@@ -1,8 +1,10 @@
 from django.db.models import Q
 from rest_framework import viewsets, status
+from rest_framework.utils.serializer_helpers import ReturnDict
 from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.serializers import BaseSerializer
 from rest_framework.decorators import action
 from post.models import Post
 from post.serializers import PostBoardSerializer
@@ -22,6 +24,44 @@ else:
 class PostViewSet(_GenereicViewSet):
     queryset = Post.objects.all()
     serializer_class = CommentSerializer
+
+    def get_serializer_class(self) -> type[BaseSerializer[Post]]:
+        if self.action == "list":
+            return PostBoardSerializer
+        elif self.action == "comment":
+            return CommentSerializer
+        return self.serializer_class
+
+    # TODO: Need to be optimized
+    def list(self, request: Request) -> Response:
+        user_club = UserClub.objects.filter(Q(user_id=request.user.id))
+        user_club_auth: list[tuple[int, str]] = [
+            (userclub.club_id, userclub.auth) for userclub in user_club
+        ]
+        response: list[ReturnDict] = []
+        for club_id, auth in user_club_auth:
+            post = (
+                self.get_queryset()
+                .select_related("event")
+                .prefetch_related("post_tag_set__tag__tag_user_set")
+                .filter(club_id=club_id)
+            )
+            if auth == "A":
+                response.append(
+                    self.get_serializer(
+                        post, many=True, context={"id": request.user.id}
+                    ).data
+                )
+            elif auth == "M":
+                all_post = self.get_serializer(
+                    post, many=True, context={"id": request.user.id}
+                ).data
+                response.extend(
+                    filter(lambda post: len(post["include_tag"]) != 0, all_post)
+                )
+            else:
+                return Response()
+        return Response(response)
 
     @action(detail=True, methods=["GET", "POST"])
     def comment(self, request: Request, pk: Any) -> Response:
