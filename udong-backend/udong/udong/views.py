12caedc8cccca57d8b8e305d7880c2b4bf92e690
Donghyun Koh <33983582+kohandy1902@users.noreply.github.com>
2022-11-22 00:25:32 +0900

    :ping_pong: Fix csrf warning (#162)

--- udong-backend/udong/udong/views.py
+++ udong-backend/udong/udong/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpRequest
+from django.views.decorators.http import require_GET
 
 
+@require_GET
 def ping(request: HttpRequest) -> HttpResponse:
     return HttpResponse("pong")
