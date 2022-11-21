99e9f9babcff3772bf9fc089e2f159a05798f9f5
JiyeonP <q0o0p@snu.ac.kr>
2022-11-21 14:26:42 +0900

    add enrollment url

--- udong-backend/udong/post/urls.py
+++ udong-backend/udong/post/urls.py
@@ -1,10 +1,12 @@
 from rest_framework.routers import SimpleRouter
 from post.views import PostViewSet, PostClubViewSet
+from udong.post.views import EnrollmentViewSet
 
 app_name = "post"
 
 router = SimpleRouter()
 router.register("post", PostViewSet)
 router.register("post/club", PostClubViewSet)
+router.register("enroll", EnrollmentViewSet)
 
 urlpatterns = router.urls
