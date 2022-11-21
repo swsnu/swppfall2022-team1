ba6b4827f433f0f31f45c04b246b70e1d7968cf3
Donghyun Koh <andy1902@snu.ac.kr>
2022-11-22 06:28:22 +0900

    Add AuthSerializer

--- udong-backend/udong/user/serializers.py
+++ udong-backend/udong/user/serializers.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import authenticate, login
 from rest_framework import serializers
 from user.models import User
+from typing import Dict, Any
 
 
 class UserSerializer(serializers.ModelSerializer[User]):
@@ -18,3 +20,32 @@ class UserSerializer(serializers.ModelSerializer[User]):
             "created_at",
             "updated_at",
         )
+
+
+class AuthSerializer(serializers.ModelSerializer[User]):
+    token = serializers.CharField(write_only=True)
+    image = serializers.CharField(default="", read_only=True)
+    time_table = serializers.CharField(default="", read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "image",
+            "email",
+            "token",
+            "time_table",
+            "name",
+            "created_at",
+            "updated_at",
+        )
+
+    def create(self, validated_data: Dict[str, Any]) -> User:
+        try:
+            user = User.objects.get(email=validated_data["email"])
+        except:
+            user = User.objects.create_user(**validated_data)
+        login(self.context["request"], user)
+        return User()
