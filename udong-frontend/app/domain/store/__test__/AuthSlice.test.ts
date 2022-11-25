91c3f14688d1bbc4b0985762567ad0e124142c77
JiyeonP <q0o0p@snu.ac.kr>
2022-11-26 00:33:52 +0900

    add authslice test

---
+++ udong-frontend/app/domain/store/__test__/AuthSlice.test.ts
@@ -0,0 +1,21 @@
+import { authReducer } from '../auth/AuthSlice'
+
+describe('auth slice', () => {
+    // let store: EnhancedStore<{ isLoggedIn: AuthState },
+    //     AnyAction,
+    //     [ThunkMiddleware<{ isLoggedIn: AuthState }, AnyAction, undefined>]>
+    // const fakeAuth = {
+    //     isLoggedIn: true,
+    // }
+
+    beforeAll(() => {
+        // store = configureStore({ reducer: { isLoggedIn: authReducer } })
+    })
+    it('should handle initial state', () => {
+        expect(authReducer(undefined, { type: 'unknown' })).toEqual({
+            isLoggedIn: false,
+        })
+    })
+    
+    //should be implemented later
+})
