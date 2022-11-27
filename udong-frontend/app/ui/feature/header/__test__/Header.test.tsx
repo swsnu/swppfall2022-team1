9344d84104f1427431500099bfa412cd3bc143cf
yblee2001 <yblee2001@gmail.com>
2022-11-27 20:39:09 +0900

    fix tests

--- udong-frontend/app/ui/feature/header/__test__/Header.test.tsx
+++ udong-frontend/app/ui/feature/header/__test__/Header.test.tsx
@@ -1,12 +1,57 @@
+import { configureStore } from '@reduxjs/toolkit'
 import { fireEvent, render, screen } from '@testing-library/react'
+import { Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
 import * as router from 'next/router'
 import { NextRouter } from 'next/router'
+import { Provider } from 'react-redux'
 
+import { authReducer, AuthState } from '../../../../domain/store/auth/AuthSlice'
 import { Header, HEADER_PAGE } from '../Header'
 
+jest.mock('next/config', () => () => ({
+    publicRuntimeConfig: {
+        backendUrl: '',
+    },
+}))
+
+const stubAuthInitialState: AuthState = {
+    isLoggedIn: true,
+}
+
+const mockStore = configureStore({
+    reducer: { auth: authReducer },
+    preloadedState: { auth: stubAuthInitialState },
+})
+
+const mockSession: Session = {
+    expires: '',
+    user: { email: '', id: '', name: '' },
+    accessToken: '',
+}
+
 describe('<Header/>', () => {
+    const header: JSX.Element = (
+        <SessionProvider session={mockSession}>
+            <Provider store={mockStore}>
+                <Header type={HEADER_PAGE.MAIN}/>
+            </Provider>
+        </SessionProvider>
+    )
+
+    const clubHeader: JSX.Element = (
+        <SessionProvider session={mockSession}>
+            <Provider store={mockStore}>
+                <Header
+                    type={HEADER_PAGE.CLUB}
+                    clubId={1}
+                />
+            </Provider>
+        </SessionProvider>
+    )
+
     it('should render header', () => {
-        render(<Header type={HEADER_PAGE.MAIN}/>)
+        render(header)
         const text = screen.getByText('로그아웃')
         expect(text).toBeDefined()
     })
@@ -17,7 +62,7 @@ describe('<Header/>', () => {
             push: (url: string) => mockPush(url),
         } as unknown as NextRouter))
 
-        render(<Header type={HEADER_PAGE.MAIN}/>)
+        render(header)
         const component = screen.getAllByRole('img')[0]
         fireEvent.click(component)
         expect(mockPush).toHaveBeenCalledWith('/')
@@ -30,10 +75,7 @@ describe('<Header/>', () => {
             push: (url: string) => mockPush(url),
         } as unknown as NextRouter))
 
-        render(<Header
-            type={HEADER_PAGE.CLUB}
-            clubId={1}
-        />)
+        render(clubHeader)
         const component = screen.getByText('단풍')
         fireEvent.click(component)
         expect(mockPush).toHaveBeenCalledWith('/club/1')
