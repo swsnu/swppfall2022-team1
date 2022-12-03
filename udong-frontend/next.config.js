1785449e1bb4422d993bc87021d1556605f98349
Yujin Im <jina0605@gmail.com>
2022-12-04 02:41:45 +0900

    Update build command

--- udong-frontend/next.config.js
+++ udong-frontend/next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     publicRuntimeConfig: {
         backendUrl: process.env.NEXT_PUBLIC_BASE_URL,
     },
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
