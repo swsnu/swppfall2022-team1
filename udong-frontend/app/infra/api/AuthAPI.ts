b7266fe9c88957b8dcba49a60dd8b3acd58f1da6
yblee2001 <yblee2001@gmail.com>
2022-12-07 03:51:53 +0900

    fix: necessary settings

--- udong-frontend/app/infra/api/AuthAPI.ts
+++ udong-frontend/app/infra/api/AuthAPI.ts
@@ -4,6 +4,7 @@ export const AuthAPI = (() => {
     async function signIn(email: string, token: string, name: string): Promise<void> {
         axios.defaults.xsrfCookieName = 'csrftoken'
         axios.defaults.xsrfHeaderName = 'X-Csrftoken'
+        axios.defaults.withCredentials = true
 
         return await axios.post(
             process.env.NEXT_PUBLIC_BASE_URL + `/api/auth/signin/`,
--- udong-frontend/app/infra/global.ts
+++ udong-frontend/app/infra/global.ts
@@ -3,8 +3,8 @@ import axios from 'axios'
 export const axiosConfig = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL,
     withCredentials: true,
-    // xsrfCookieName: 'csrftoken',
-    // xsrfHeaderName: 'X-Csrftoken',
+    xsrfCookieName: 'csrftoken',
+    xsrfHeaderName: 'X-Csrftoken',
 })
 // axiosConfig.defaults.xsrfCookieName = 'csrftoken'
 // axiosConfig.defaults.xsrfHeaderName = 'X-CSRFToken'
