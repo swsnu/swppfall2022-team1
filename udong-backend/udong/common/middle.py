dcea60376af2aca8ed24232eefb896ba60d5fcf3
Yujin Im <jina0605@gmail.com>
2022-12-08 05:35:42 +0900

    Add typing

--- udong-backend/udong/common/middle.py
+++ udong-backend/udong/common/middle.py
@@ -1,8 +1,18 @@
+from typing import Any, Protocol
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+
+
+class GetResponseCallable(Protocol):
+    def __call__(self, __request: HttpRequest) -> HttpResponse:
+        ...
+
+
 class DisableCSRFMiddleware(object):
-    def __init__(self, get_response):
+    def __init__(self, get_response: GetResponseCallable) -> None:
         self.get_response = get_response
 
-    def __call__(self, request):
+    def __call__(self, request: HttpRequest) -> HttpResponse:
         setattr(request, "_dont_enforce_csrf_checks", True)
         response = self.get_response(request)
         return response
