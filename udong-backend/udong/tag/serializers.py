ad67d150ed35bee8731735a5a09334bb3d6f2923
yblee2001 <yblee2001@gmail.com>
2022-12-08 23:58:34 +0900

    fix: tag api 성공적

--- udong-backend/udong/tag/serializers.py
+++ udong-backend/udong/tag/serializers.py
@@ -5,27 +5,6 @@ from tag.models import Tag, UserTag
 from user.serializers import UserSerializer
 
 
-class TagUserSerializer(serializers.ModelSerializer[UserTag]):
-    name = serializers.CharField(max_length=255)
-    users = serializers.SerializerMethodField(read_only=True)
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
-
-    class Meta:
-        model = UserTag
-        fields = (
-            "id",
-            "name",
-            "users",
-            "created_at",
-            "updated_at",
-        )
-
-    @swagger_serializer_method(serializer_or_field=UserSerializer(many=True))
-    def get_users(self, user_tag: UserTag) -> ReturnDict:
-        return UserSerializer(user_tag.user, many=True).data
-
-
 class TagPostSerializer(serializers.ModelSerializer[Tag]):
     name = serializers.CharField(max_length=255)
 
@@ -35,3 +14,25 @@ class TagPostSerializer(serializers.ModelSerializer[Tag]):
             "id",
             "name",
         )
+
+
+class TagUserSerializer(serializers.ModelSerializer[Tag]):
+    name = serializers.CharField(max_length=255)
+    users = serializers.SerializerMethodField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = Tag
+        fields = (
+            "id",
+            "name",
+            "users",
+            "created_at",
+            "updated_at",
+        )
+
+    @swagger_serializer_method(serializer_or_field=UserSerializer(many=True))
+    def get_users(self, tag: Tag) -> ReturnDict:
+        users = list(map(lambda user_tag: user_tag.user, tag.tag_user_set.all()))
+        return UserSerializer(users, many=True).data
--- udong-backend/udong/tag/views.py
+++ udong-backend/udong/tag/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.request import Request
 from rest_framework.response import Response
-from tag.models import UserTag
+from tag.models import Tag
 from tag.serializers import TagUserSerializer
 from typing import TYPE_CHECKING, Any
 
 # Create your views here.
 
 if TYPE_CHECKING:
-    _GenericViewSet = viewsets.GenericViewSet[UserTag]
+    _GenericViewSet = viewsets.GenericViewSet[Tag]
 else:
     _GenericViewSet = viewsets.GenericViewSet
 
 
 class TagViewSet(_GenericViewSet):
-    queryset = UserTag.objects.all()
+    queryset = Tag.objects.all()
     serializer_class = TagUserSerializer
 
     def retrieve(self, request: Request, pk: Any = None) -> Response:
