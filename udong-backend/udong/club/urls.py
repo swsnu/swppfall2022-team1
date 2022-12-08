9151f37857fafd5f6c2bf8f0fc4adfeba8f10533
Donghyun Koh <andy1902@snu.ac.kr>
2022-12-09 00:10:21 +0900

    Set url for ClubUserViewset

--- udong-backend/udong/club/urls.py
+++ udong-backend/udong/club/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import SimpleRouter
-from club.views import ClubViewSet
+from club.views import ClubViewSet, ClubUserViewSet
 
 app_name = "club"
 
 router = SimpleRouter()
 router.register("club", ClubViewSet)
+router.register(r"club/(?P<club_id>[^/.]+)/user", ClubUserViewSet)
 
 urlpatterns = router.urls
