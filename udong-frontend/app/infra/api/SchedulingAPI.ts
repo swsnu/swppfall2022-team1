911e503212328eb7007df4310c44234397540a4c
Yujin Im <jina0605@gmail.com>
2022-12-09 03:35:09 +0900

    Implement SchedulingAPI

--- udong-frontend/app/infra/api/SchedulingAPI.ts
+++ udong-frontend/app/infra/api/SchedulingAPI.ts
@@ -1,5 +1,13 @@
+import { SchedulingPost } from '../../domain/model/SchedulingPost'
+import { SchedulingDto } from '../dto/SchedulingDto'
+import { axiosConfig } from '../global'
+import { schedulingTransformer } from '../transformer/SchedulingTransformer'
+
 export const SchedulingAPI = (() => {
-    function getSchedulingStatus() { return }
+    async function getSchedulingStatus(postId: number): Promise<SchedulingPost> {
+        const response = await axiosConfig.get<SchedulingDto>(`/api/schedule/${postId}/status/`)
+        return schedulingTransformer.fromDto(response.data)
+    }
     function closeScheduling() { return }
 
     return Object.freeze({
