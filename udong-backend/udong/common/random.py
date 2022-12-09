6e43365aa28c2417139744cf21a5bd9684a0fea7
Donghyun Koh <andy1902@snu.ac.kr>
2022-12-10 01:34:52 +0900

    Add random module

---
+++ udong-backend/udong/common/random.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import os
+import environ  # type: ignore[import]
+import secrets
+
+
+env = environ.Env(
+    DEFAULT_IMAGES=(list, []),
+)
+BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+
+class Random:
+    @staticmethod
+    def choice_image() -> str:
+        return secrets.choice(env("DEFAULT_IMAGES"))  # type: ignore
