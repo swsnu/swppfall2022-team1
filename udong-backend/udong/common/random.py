9e53e6458dce37ae3d6f31b6476301477cef6a2d
Donghyun Koh <andy1902@snu.ac.kr>
2022-12-10 04:38:00 +0900

    Add generate_code function

--- udong-backend/udong/common/random.py
+++ udong-backend/udong/common/random.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 import environ  # type: ignore[import]
 import secrets
+import string
 
 
 env = environ.Env(
@@ -15,3 +16,9 @@ class Random:
     @staticmethod
     def choice_image() -> str:
         return secrets.choice(env("DEFAULT_IMAGES"))  # type: ignore
+
+    @staticmethod
+    def generate_code(size: int = 10) -> str:
+        return "".join(
+            secrets.choice(string.ascii_letters + string.digits) for _ in range(size)
+        )
