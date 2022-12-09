bf591570bfe8984d4856f55b33c3367af23720ad
Donghyun Koh <33983582+kohandy1902@users.noreply.github.com>
2022-12-10 04:48:11 +0900

    :game_die: Back/feat/random code (#252)

--- udong-backend/udong/club/serializers.py
+++ udong-backend/udong/club/serializers.py
@@ -1,5 +1,3 @@
-import string
-import random
 from rest_framework import serializers
 from club.models import Club
 from user.models import UserClub
@@ -26,14 +24,11 @@ class ClubSerializer(serializers.ModelSerializer[Club]):
         )
 
     def create(self, validated_data: Dict[str, Any]) -> Club:
-        code = "".join(
-            random.choice(string.ascii_letters + string.digits) for _ in range(10)
-        )
         if "image" in validated_data:
             del validated_data["image"]
 
         club = Club.objects.create(
-            **validated_data, code=code, image=Random.choice_image()
+            **validated_data, code=Random.generate_code(), image=Random.choice_image()
         )
         UserClub.objects.create(user=self.context["user"], club=club, auth="A")
         tag = Tag.objects.create(club=club, name="전체", is_default=True)
