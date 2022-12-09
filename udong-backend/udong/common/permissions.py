0230ad5fada0525907aa32e18127c38aee96ad58
Donghyun Koh <andy1902@snu.ac.kr>
2022-12-10 04:27:42 +0900

    Add CanRead Permission

--- udong-backend/udong/common/permissions.py
+++ udong-backend/udong/common/permissions.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from club.models import Club
 from user.models import UserClub
 from comment.models import Comment
+from post.models import Post, Enrollment, Scheduling
 from typing import Any
 
 
@@ -28,3 +29,29 @@ class IsAuthor(BasePermission):
         if not isinstance(obj, Comment):
             return True
         return request.user.id == obj.user_id
+
+
+class CanReadPost(BasePermission):
+    def has_object_permission(self, request: Request, view: APIView, obj: Any) -> bool:
+        if isinstance(obj, Enrollment):
+            obj = obj.post
+        if isinstance(obj, Scheduling):
+            obj = obj.post
+        if isinstance(obj, Post):
+            try:
+                user_club = UserClub.objects.get(
+                    Q(user_id=request.user.id) & Q(club_id=obj.club_id)
+                )
+                if user_club.auth == "A":
+                    return True
+                return (
+                    obj.post_tag_set.select_related("tag")
+                    .prefetch_related("post_tag_set__tag__tag_user_set")
+                    .filter(tag__tag_user_set__user__id=request.user.id)
+                    .count()
+                    > 0
+                )
+            except:
+                return False
+        else:
+            return True
