6fa344a31c05817e1b072485311bd33ac33df085
Donghyun Koh <andy1902@snu.ac.kr>
2022-12-10 01:35:17 +0900

    Make set random image when club is created

--- udong-backend/udong/club/serializers.py
+++ udong-backend/udong/club/serializers.py
@@ -5,11 +5,12 @@ from club.models import Club
 from user.models import UserClub
 from tag.models import Tag, UserTag
 from typing import Dict, Any
+from common.random import Random
 
 
 class ClubSerializer(serializers.ModelSerializer[Club]):
     code = serializers.CharField(max_length=10, read_only=True)
-    image = serializers.CharField(default="default")
+    image = serializers.CharField(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
     updated_at = serializers.DateTimeField(read_only=True)
 
@@ -28,7 +29,9 @@ class ClubSerializer(serializers.ModelSerializer[Club]):
         code = "".join(
             random.choice(string.ascii_letters + string.digits) for _ in range(10)
         )
-        club = Club.objects.create(**validated_data, code=code)
+        club = Club.objects.create(
+            **validated_data, code=code, image=Random.choice_image()
+        )
         UserClub.objects.create(user=self.context["user"], club=club, auth="A")
         tag = Tag.objects.create(club=club, name="전체", is_default=True)
         UserTag.objects.create(user=self.context["user"], tag=tag)
