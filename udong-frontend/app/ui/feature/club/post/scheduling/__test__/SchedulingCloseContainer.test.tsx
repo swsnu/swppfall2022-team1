f1352151faffa01f097520e1afc31ac36d1e6666
Yujin Im <jina0605@gmail.com>
2022-12-09 20:13:59 +0900

    Pass SchedulingCloseContainer test

--- udong-frontend/app/ui/feature/club/post/scheduling/__test__/SchedulingCloseContainer.test.tsx
+++ udong-frontend/app/ui/feature/club/post/scheduling/__test__/SchedulingCloseContainer.test.tsx
@@ -1,10 +1,55 @@
+import { configureStore } from '@reduxjs/toolkit'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import * as router from 'next/router'
 import { NextRouter } from 'next/router'
 import { act } from 'react-dom/test-utils'
+import { Provider } from 'react-redux'
 
+import { SchedulingPostType } from '../../../../../../domain/model/SchedulingPostType'
+import { clubReducer, ClubState } from '../../../../../../domain/store/club/ClubSlice'
+import { schedulingReducer, SchedulingState } from '../../../../../../domain/store/post/scheduling/SchedulingSlice'
+import { userReducer } from '../../../../../../domain/store/user/UserSlice'
+import { new2dArray } from '../../../../../../utility/functions'
 import { SchedulingCloseContainer } from '../SchedulingCloseContainer'
 
+const schedulingInitialState: SchedulingState = {
+    isOpen: false,
+    schedulingStatus: {
+        type: SchedulingPostType.DAYS,
+        startTime: 12,
+        endTime: 18,
+        dates: [
+            new Date(2022, 11, 6),
+            new Date(2022, 11, 9),
+            new Date(2022, 11, 10),
+            new Date(2022, 11, 11),
+        ],
+        confirmedTime: null,
+        closed: false,
+        availableTime: [
+            {
+                id: 1,
+                user: { id: 1, name: 'user1', email: 'email1', imageUrl: '', timeTable: new2dArray(7, 48, false) },
+                time: [
+                    [true, true, true, true, true, true],
+                    [true, true, false, false, true, false],
+                    [true, true, false, false, true, false],
+                    [false, false, false, false, false, false],
+                ],
+            },
+        ],
+    },
+}
+
+const clubInitialState: ClubState = { myClubs: [], members: [] }
+
+const mockStore = configureStore({
+    reducer: { club: clubReducer, scheduling: schedulingReducer, user: userReducer },
+    preloadedState: {
+        scheduling: schedulingInitialState,
+        club: clubInitialState,
+    },
+})
 describe('<SchedulingCloseContainer/>', () => {
     it('renders SchedulingCloseContainer', async () => {
         const mockBack = jest.fn()
@@ -13,7 +58,13 @@ describe('<SchedulingCloseContainer/>', () => {
             back: mockBack,
         } as unknown as NextRouter))
 
-        await act(async () => {render(<SchedulingCloseContainer/>)})
+        await act(async () => {
+            render(
+                <Provider store={mockStore}>
+                    <SchedulingCloseContainer/>
+                </Provider>,
+            )
+        })
 
         const backBtn = screen.getAllByRole('img')[0]
         await waitFor(() => expect(backBtn).toBeInTheDocument())
