99c4636823ab0338f46ab61512bee725acd573ed
JiyeonP <q0o0p@snu.ac.kr>
2022-12-10 16:17:03 +0900

    fix post api error

--- udong-backend/udong/post/serializers.py
+++ udong-backend/udong/post/serializers.py
@@ -106,7 +106,7 @@ class PostBoardSerializer(serializers.ModelSerializer[Post]):
     author = serializers.SerializerMethodField()
     club = serializers.SerializerMethodField()
     event = serializers.SerializerMethodField()
-    event_id = serializers.IntegerField(write_only=True, required=False)
+    event_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
     title = serializers.CharField(max_length=255)
     type = serializers.ChoiceField(choices=["A", "E", "S"])
     closed = serializers.SerializerMethodField()
