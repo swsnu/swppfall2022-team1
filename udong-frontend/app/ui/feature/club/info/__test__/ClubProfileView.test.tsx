8262df45489334c651b033aac740611c22856303
yblee2001 <yblee2001@gmail.com>
2022-12-10 18:56:11 +0900

    fix: tests

--- udong-frontend/app/ui/feature/club/info/__test__/ClubProfileView.test.tsx
+++ udong-frontend/app/ui/feature/club/info/__test__/ClubProfileView.test.tsx
@@ -1,18 +1,53 @@
+import { configureStore } from '@reduxjs/toolkit'
 import { fireEvent, render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
 
+import { Club } from '../../../../../domain/model/Club'
+import { clubReducer, ClubState } from '../../../../../domain/store/club/ClubSlice'
 import { ClubProfileView } from '../ClubProfileView'
+
+const testClub: Club = {
+    id: 1,
+    name: '',
+    image: '',
+    code: '',
+}
+
+const stubClubInitialState: ClubState = {
+    myClubs: [],
+    members: [],
+}
+
+const mockStore = configureStore({
+    reducer: { club: clubReducer },
+    preloadedState: { club: stubClubInitialState },
+})
 
 describe('<ClubProfileView/>', () => {
     it('should test on click delete', () => {
         const mockOnDelete = jest.fn()
-        render(<ClubProfileView onClickDelete={() => {mockOnDelete()}}/>)
+        render(
+            <Provider store={mockStore}>
+                <ClubProfileView
+                    onClickDelete={() => {mockOnDelete()}}
+                    club={testClub}
+                />
+            </Provider>,
+        )
         const text = screen.getByText('삭제하기')
         fireEvent.click(text)
         expect(mockOnDelete).toHaveBeenCalledTimes(1)
     })
 
     it('should test on click leave', () => {
-        render(<ClubProfileView onClickDelete={() => {return}}/>)
+        render(
+            <Provider store={mockStore}>
+                <ClubProfileView
+                    onClickDelete={() => {return}}
+                    club={testClub}
+                />
+            </Provider>,
+        )
         const text = screen.getByText('탈퇴하기')
         fireEvent.click(text)
         expect(text).toBeDefined()
