824134cd0fa675853781d83127a1d7186f7a84f8
JiyeonP <q0o0p@snu.ac.kr>
2022-12-11 15:33:44 +0900

    make fnc for debounced search

---
+++ udong-frontend/app/utility/useDebouncedSearch.tsx
@@ -0,0 +1,20 @@
+import { Dispatch, SetStateAction, useEffect } from 'react'
+
+export function useDebouncedSearch(value: string, setKeyword: Dispatch<SetStateAction<string>>, delay:number ) {
+    // State and setters for debounced value
+    useEffect(
+        () => {
+            // Update debounced value after delay
+            const handler = setTimeout(() => {
+                setKeyword(value)
+            }, delay)
+            // Cancel the timeout if value changes (also on delay change or unmount)
+            // This is how we prevent debounced value from updating if value is changed ...
+            // .. within the delay period. Timeout gets cleared and restarted.
+            return () => {
+                clearTimeout(handler)
+            }
+        },
+        [value, delay], // Only re-call effect if value or delay changes
+    )
+}
