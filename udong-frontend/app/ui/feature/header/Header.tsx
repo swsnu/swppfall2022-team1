857ce76e09985fe195dc34ff49d3d520b17d59ee
yblee2001 <yblee2001@gmail.com>
2022-12-11 18:43:15 +0900

    fix: synchronize logout

--- udong-frontend/app/ui/feature/header/Header.tsx
+++ udong-frontend/app/ui/feature/header/Header.tsx
@@ -36,9 +36,11 @@ export const Header = ({ type, clubId }: HeaderProps) => {
 
     const clubName = '단풍'
 
-    const handleLogout = useCallback(() => {
-        dispatch(authActions.logout())
-        signOut({ redirect: false })
+    const handleLogout = useCallback(async () => {
+        const response = await dispatch(authActions.logout())
+        if (response.type === `${authActions.logout.typePrefix}/fulfilled`) {
+            signOut({ redirect: false })
+        }
     }, [dispatch])
 
     useEffect(() => {
