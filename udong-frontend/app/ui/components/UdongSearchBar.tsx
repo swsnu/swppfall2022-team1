9414d62bd4a4418e5627399d231c452ac83c2cb9
JiyeonP <q0o0p@snu.ac.kr>
2022-12-11 15:47:11 +0900

    remove search icon

--- udong-frontend/app/ui/components/UdongSearchBar.tsx
+++ udong-frontend/app/ui/components/UdongSearchBar.tsx
@@ -1,10 +1,8 @@
-import IcSearch from '../icons/IcSearch.png'
 import { UdongTextField, UdongTextFieldProps } from './UdongTextField'
 
 export const UdongSearchBar = (props: UdongTextFieldProps) => {
     return <UdongTextField
         placeholder={'검색어를 입력해주세요'}
-        imageSrc={IcSearch.src}
         {...props}
     />
 }
