39031465d1ea7b9773f8d0fbd4236c09abca8544
JiyeonP <q0o0p@snu.ac.kr>
2022-12-14 05:48:58 +0900

    fix type err

--- udong-frontend/app/infra/dto/TimeDto.ts
+++ udong-frontend/app/infra/dto/TimeDto.ts
@@ -4,9 +4,9 @@ export interface TimeDto {
     type: SchedulingPostType
     start_date: string
     end_date: string
-    repeat_start?: string
-    repeat_end?: string
-    weekday?: number
+    repeat_start: string
+    repeat_end: string
+    weekday: number
     start_time: number
     end_time: number
     created_at: string
