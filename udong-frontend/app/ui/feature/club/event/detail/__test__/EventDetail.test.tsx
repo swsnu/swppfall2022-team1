407018e95eff05cfe0f2a2b9dbb1a51d08d2e3df
Yujin Im <jina0605@gmail.com>
2022-12-14 07:37:13 +0900

    Pass test

--- udong-frontend/app/ui/feature/club/event/detail/__test__/EventDetail.test.tsx
+++ udong-frontend/app/ui/feature/club/event/detail/__test__/EventDetail.test.tsx
@@ -1,13 +1,65 @@
+import { configureStore } from '@reduxjs/toolkit'
 import { fireEvent, render, screen } from '@testing-library/react'
 import * as router from 'next/router'
 import { NextRouter } from 'next/router'
+import { Provider } from 'react-redux'
 
+import { PostDisplayType } from '../../../../../../domain/model/BoardPost'
+import { PostType } from '../../../../../../domain/model/PostType'
+import { eventReducer, EventState } from '../../../../../../domain/store/event/EventSlice'
+import { postReducer, PostState } from '../../../../../../domain/store/post/PostSlice'
 import { EventDetailContainer } from '../EventDetailContainer'
+
+const eventInitialState: EventState = {
+    events: [],
+    selectedEvent: {
+        id: 1,
+        name: 'event name',
+        createdAt: '',
+        updatedAt: '',
+        times: [],
+    },
+    errors: {},
+}
+
+const postInitialState: PostState = {
+    selectedPost: {
+        displayType: PostDisplayType.CLUB,
+        id: 1,
+        title: 'title',
+        content: 'content',
+        type: PostType.ANNOUNCEMENT,
+        eventName: {
+            id: 1,
+            name: 'event',
+        },
+    },
+    feedPosts: [],
+    clubPosts: [],
+    errors: {},
+    eventPosts: [],
+}
+
+export const mockStore = configureStore({
+    reducer: { event: eventReducer, post: postReducer },
+    preloadedState: { event: eventInitialState, post: postInitialState },
+})
+
+jest.mock('next/config', () => () => ({
+    publicRuntimeConfig: {
+        backendUrl: '',
+    },
+}))
 
 describe('<EventDetailContainer/>', () => {
     it ('should render event detail container', () => {
-        render(<EventDetailContainer/>)
-        const component = screen.getByText('2022년 겨울 공연')
+        const mockBack = jest.fn()
+        jest.spyOn(router, 'useRouter').mockImplementation(() => ({
+            back: () => mockBack(),
+            query: ({ clubId: 1, eventId: 1 }),
+        } as unknown as NextRouter))
+        render(<Provider store={mockStore}><EventDetailContainer/></Provider>)
+        const component = screen.getByText('event name')
         expect(component).toBeDefined()
     })
 
@@ -15,9 +67,10 @@ describe('<EventDetailContainer/>', () => {
         const mockBack = jest.fn()
         jest.spyOn(router, 'useRouter').mockImplementation(() => ({
             back: () => mockBack(),
+            query: ({ clubId: 1, eventId: 1 }),
         } as unknown as NextRouter))
 
-        render(<EventDetailContainer/>)
+        render(<Provider store={mockStore}><EventDetailContainer/></Provider>)
         const component = screen.getAllByRole('img')[0]
         fireEvent.click(component)
         expect(mockBack).toHaveBeenCalledTimes(1)
@@ -27,16 +80,22 @@ describe('<EventDetailContainer/>', () => {
         const mockPush = jest.fn()
         jest.spyOn(router, 'useRouter').mockImplementation(() => ({
             push: (url: string) => mockPush(url),
+            query: ({ clubId: 1, eventId: 1 }),
         } as unknown as NextRouter))
 
-        render(<EventDetailContainer/>)
+        render(<Provider store={mockStore}><EventDetailContainer/></Provider>)
         const component = screen.getByText('수정하기')
         fireEvent.click(component)
         expect(mockPush).toHaveBeenCalledWith('/club/1/event/1/edit')
     })
 
     it ('should handle on click delete', () => {
-        render(<EventDetailContainer/>)
+        const mockPush = jest.fn()
+        jest.spyOn(router, 'useRouter').mockImplementation(() => ({
+            push: (url: string) => mockPush(url),
+            query: ({ clubId: 1, eventId: 1 }),
+        } as unknown as NextRouter))
+        render(<Provider store={mockStore}><EventDetailContainer/></Provider>)
         const component = screen.getByText('삭제하기')
         fireEvent.click(component)
         expect(component).toBeDefined()
