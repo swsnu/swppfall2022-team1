040afa9a77445cd024b4ec891ba92ae88382c9eb
Yujin Im <jina0605@gmail.com>
2022-12-14 05:18:16 +0900

    Update useImage.ts

--- udong-frontend/app/hooks/useImage.ts
+++ udong-frontend/app/hooks/useImage.ts
@@ -3,9 +3,16 @@ import { useQuery } from 'react-query'
 import { axiosConfig } from '../infra/global'
 
 export const useImage = (imgKey: string) => {
-    const { data } = useQuery<string>(imgKey, async () => {
-        const response = await axiosConfig.get(`/api/image/download/?key=${imgKey}`)
-        return response.data
-    }, { staleTime: 200 * 1000 })
+    const { data } = useQuery<string, unknown, string, string>(
+        imgKey,
+        async ({ queryKey } : { queryKey: string[] }) => {
+            if(!queryKey[0]) {
+                return ''
+            }
+            const response = await axiosConfig.get(`/api/image/download/?key=${queryKey[0]}`)
+            return response.data
+        },
+        { staleTime: 200 * 1000 },
+    )
     return data
 }
