bd07ec73b186426a2a3a2539a494b7a547a83a11
JiyeonP <66158942+JiyeonP@users.noreply.github.com>
2022-12-14 17:21:45 +0900

    auth for event update & delete (#312)
    Co-authored-by: Yujin Im <jina0605@gmail.com>

--- udong-backend/udong/event/views.py
+++ udong-backend/udong/event/views.py
@@ -5,6 +5,8 @@ from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.serializers import BaseSerializer
+from rest_framework.permissions import IsAuthenticated
+from common.permissions import IsAdmin
 from event.models import Event
 from post.models import Post
 from event.serializers import ClubEventSerializer
@@ -15,8 +17,12 @@ from post.serializers import PostBoardSerializer
 
 if TYPE_CHECKING:
     _GenericViewSet = viewsets.GenericViewSet[Event]
+    from rest_framework.permissions import _SupportsHasPermission
+
+    _SupportsHasPermissionType = list[_SupportsHasPermission]
 else:
     _GenericViewSet = viewsets.GenericViewSet
+    _SupportsHasPermissionType = list
 
 _MT_co = TypeVar("_MT_co", bound=Model, covariant=True)
 
@@ -24,6 +30,11 @@ _MT_co = TypeVar("_MT_co", bound=Model, covariant=True)
 class EventViewSet(_GenericViewSet):
     queryset = Event.objects.all()
     serializer_class = ClubEventSerializer
+
+    def get_permissions(self) -> _SupportsHasPermissionType:
+        if self.action in ("update", "destroy"):
+            return [IsAuthenticated(), IsAdmin()]
+        return super().get_permissions()
 
     def get_serializer_class(self) -> type[BaseSerializer[_MT_co]]:
         if self.action in ("retrieve", "update", "destroy"):
@@ -36,6 +47,16 @@ class EventViewSet(_GenericViewSet):
         return Response(self.get_serializer(self.get_object()).data)
 
     def update(self, request: Request, pk: Any) -> Response:
+        club = self.get_object().club
+
+        obj_permission = IsAdmin()
+        if not obj_permission.has_object_permission(request, self, club):
+            self.permission_denied(
+                request,
+                message=getattr(obj_permission, "message", None),
+                code=getattr(obj_permission, "code", None),
+            )
+
         event = self.get_object()
         serializer = self.get_serializer(event, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
@@ -43,6 +64,16 @@ class EventViewSet(_GenericViewSet):
         return Response(serializer.data)
 
     def destroy(self, request: Request, pk: Any) -> Response:
+        club = self.get_object().club
+
+        obj_permission = IsAdmin()
+        if not obj_permission.has_object_permission(request, self, club):
+            self.permission_denied(
+                request,
+                message=getattr(obj_permission, "message", None),
+                code=getattr(obj_permission, "code", None),
+            )
+
         event = self.get_object()
         event.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
