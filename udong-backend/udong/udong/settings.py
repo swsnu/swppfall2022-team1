72d30dd9635f830fabd28dbe0463f99d5bd29df0
Yujin Im <jina0605@gmail.com>
2022-12-17 17:56:17 +0900

    Fix csrf

--- udong-backend/udong/udong/settings.py
+++ udong-backend/udong/udong/settings.py
@@ -94,7 +94,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "common.middle.DisableCSRFMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
